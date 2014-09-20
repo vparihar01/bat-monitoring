191a9713da78f0b68540eb70b04c0577bf4f3abb
Kunal Bhatia <kunalb@weboniselab.com>
2014-09-21 05:03:34 +0530

    cahnged routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
   def index
     @hosts_status = Notification.get_notifications(nil)
-    render :json => @hosts_status
+    # render :json => @hosts_status
   end
 end
