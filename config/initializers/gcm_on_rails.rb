be388ef3614150f48b4ca12af3b904c178d4ab8e
Vivek Parihar <vivek@weboniselab.com>
2014-09-21 02:35:04 +0530

    dash board page with project listing and status report

---
+++ config/initializers/gcm_on_rails.rb
@@ -0,0 +1,4 @@
+#configutron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
+#configatron.gcm_on_rails.api_key = 'AIzaSyDRhJiLxnTCoT6ztIayXzyzFApIsVM_khM'
+#configatron.gcm_on_rails.app_name = 'BatMonitoring'
+#configatron.gcm_on_rails.delivery_format = 'json'
