38e17a9c8d37a229da97ed9c57d54e7e72dee661
Vivek Parihar <vivek@weboniselab.com>
2014-09-21 02:52:49 +0530

    Integration

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -2,5 +2,6 @@ class HostsController < ApplicationController
   before_action :authenticate_user!
   def index
     @hosts = Notification.distinct(:nagios_hostname)
+    @hosts_status = Notification.get_notifications(nil)
   end
 end
